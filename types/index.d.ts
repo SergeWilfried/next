53c832eb5e98cb500361729cb1cb1a2c92766d80
SergeWilfried <63460822+SergeWilfried@users.noreply.github.com>
2024-08-01 15:35:07 +0000

    Update index.d.ts

--- types/index.d.ts
+++ types/index.d.ts
@@ -24,7 +24,19 @@ export type NavItem = {
   authorizeOnly?: UserRole;
   icon?: keyof typeof Icons;
 };
+export interface Option {
+  label: string
+  value: string
+  icon?: React.ComponentType<{ className?: string }>
+  withCount?: boolean
+}
 
+export interface DataTableFilterField<TData> {
+  label: string
+  value: keyof TData
+  placeholder?: string
+  options?: Option[]
+}
 export type MainNavItem = NavItem;
 
 export type MarketingConfig = {
