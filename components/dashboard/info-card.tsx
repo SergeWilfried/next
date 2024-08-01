683ceb50eb257eaf1fc93958b3ea62853c494628
SergeWilfried <63460822+SergeWilfried@users.noreply.github.com>
2024-08-01 14:55:20 +0000

    update info-card.tsx

--- components/dashboard/info-card.tsx
+++ components/dashboard/info-card.tsx
@@ -1,4 +1,4 @@
-import { Users } from "lucide-react"
+import { School, Users, DollarSign, PiggyBank, UserPlus } from "lucide-react"
 
 import {
   Card,
@@ -7,16 +7,34 @@ import {
   CardTitle,
 } from "@/components/ui/card"
 
-export default function InfoCard() {
+interface InfoCardProps {
+  title: string;
+  value: string;
+  type: 'students' | 'teachers' | 'accounting' | 'donations' | 'enrollments';
+  change?: string;
+}
+
+const iconMap = {
+  students: Users,
+  teachers: School,
+  accounting: DollarSign,
+  donations: PiggyBank,
+  enrollments: UserPlus,
+}
+
+
+export default function InfoCard({ title, value, type, change }: InfoCardProps) {
+  const Icon = iconMap[type]
+
   return (
     <Card>
       <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-        <CardTitle className="text-sm font-medium">Subscriptions</CardTitle>
-        <Users className="size-4 text-muted-foreground" />
+        <CardTitle className="text-sm font-medium">{title}</CardTitle>
+        <Icon className="size-4 text-muted-foreground" />
       </CardHeader>
       <CardContent>
-        <div className="text-2xl font-bold">+2350</div>
-        <p className="text-xs text-muted-foreground">+180.1% from last month</p>
+        <div className="text-2xl font-bold">{value}</div>
+        {change && <p className="text-xs text-muted-foreground">{change}</p>}
       </CardContent>
     </Card>
   )
