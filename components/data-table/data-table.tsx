1297195750591748e49039d1965bb20f4d324381
SergeWilfried <willytheigeek@gmail.com>
2024-08-07 17:15:03 +0000

    fix: update value case

--- components/data-table/data-table.tsx
+++ components/data-table/data-table.tsx
@@ -216,11 +216,11 @@ export function DataTable<TData, TValue>({
 
 function getStatusColor(status: string) {
   switch (status.toLowerCase()) {
-    case 'ACTIVE':
-      return 'bg-green-100 text-green-800';
-    case 'INACTIVE':
+    case 'active':
+      return 'bg-green-100 text-green-8000';
+    case 'inactive':
       return 'bg-red-100 text-red-800';
-    case 'GRADUATED':
+    case 'graduated':
       return 'bg-yellow-100 text-yellow-800';
     default:
       return 'bg-gray-100 text-gray-800';
