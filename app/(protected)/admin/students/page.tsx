cf6678fccfeaf02f9ef8acc1896e873ce2851faa
SergeWilfried <willytheigeek@gmail.com>
2024-08-07 16:20:12 +0000

    fix: refactor db schema

--- app/(protected)/admin/students/page.tsx
+++ app/(protected)/admin/students/page.tsx
@@ -25,7 +25,7 @@ export default async function StudentsPage({searchParams}: {searchParams: GetStu
 
   if (!user || user.role !== "ADMIN") redirect("/login");
 
-
+  console.log(students)
   return (
     <>
       <DashboardHeader
