dbc501ea730c464bf57a37ee3dcfa70168458ae2
SergeWilfried <willytheigeek@gmail.com>
2024-08-08 09:44:53 +0000

    fix: typo

--- actions/get-applications.ts
+++ actions/get-applications.ts
@@ -1,4 +1,3 @@
-"use server";
 import { prisma } from "@/lib/db";
 
 export async function getApplications() {
--- app/(protected)/admin/reports/page.tsx
+++ app/(protected)/admin/reports/page.tsx
@@ -4,6 +4,9 @@ import { constructMetadata } from "@/lib/utils";
 import { DashboardHeader } from "@/components/dashboard/header";
 import { EmptyPlaceholder } from "@/components/shared/empty-placeholder";
 import { Button } from "@/components/ui/button";
+import { getAllReports } from "@/actions/get-reports";
+import { DataTable } from "@/components/data-table/data-table";
+import { reportsTableColumns } from "./columns";
 
 export const metadata = constructMetadata({
   title: "Reports – School Management System",
@@ -14,20 +17,28 @@ export default async function ReportsPage() {
   const user = await getCurrentUser();
   if (!user || user.role !== "ADMIN") redirect("/login");
 
+
+  // Fetch reports from the server
+  const { data: reports, error, count } = await getAllReports();
   return (
     <>
       <DashboardHeader
         heading="Reports"
         text="Manage reports in the school system."
       />
-      <EmptyPlaceholder>
-        <EmptyPlaceholder.Icon name="file" />
+      {error && <div>{error}</div>}
+      {reports.length === 0 ? (
+        <EmptyPlaceholder>
+          <EmptyPlaceholder.Icon name="file" />
         <EmptyPlaceholder.Title>No reports listed</EmptyPlaceholder.Title>
         <EmptyPlaceholder.Description>
           You don&apos;t have any reports yet. Start by adding some.
         </EmptyPlaceholder.Description>
         <Button>Add Reports</Button>
       </EmptyPlaceholder>
+    ) : (
+      <DataTable data={reports} columns={reportsTableColumns} pageCount={count} />
+    )}
     </>
   );
 }
