e7f1d73bf0fb8474b8a939a68b662090d4ca86d0
SergeWilfried <willytheigeek@gmail.com>
2024-09-23 20:32:45 +0000

    feat: disable marketing navigation

--- config/marketing.ts
+++ config/marketing.ts
@@ -2,17 +2,6 @@ import { MarketingConfig } from "types"
 
 export const marketingConfig: MarketingConfig = {
   mainNav: [
-    {
-      title: "Pricing",
-      href: "/pricing",
-    },
-    {
-      title: "Blog",
-      href: "/blog",
-    },
-    {
-      title: "Documentation",
-      href: "#",
-    },
+
   ],
 }
