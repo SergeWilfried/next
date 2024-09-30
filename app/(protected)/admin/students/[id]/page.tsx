a38aacd54ff29db4137a797386c7fcd057e4a506
SergeWilfried <willytheigeek@gmail.com>
2024-09-30 17:57:06 +0000

    fix: update profile view

--- app/(protected)/admin/students/[id]/page.tsx
+++ app/(protected)/admin/students/[id]/page.tsx
@@ -1,19 +1,7 @@
-import { DashboardHeader } from "@/components/dashboard/header";
 import EnhancedStudentProfile from "@/components/user-profile/page";
 
 export default function ProfilePage({ params }: { params: { id: string } }) {
   return (
-    <>
-      <DashboardHeader
-        heading="Student Profile"
-        items={[
-          { href: "/", label: "Home" },
-          { href: "/admin", label: "Admin" },
-          { href: "/admin/students", label: "Students" },
-          { href: `/admin/students/${params.id}`, label: "Profile" },
-        ]}
-      />
-      <EnhancedStudentProfile id={params.id} />
-    </>
+    <EnhancedStudentProfile id={params.id} />
   )
 }
