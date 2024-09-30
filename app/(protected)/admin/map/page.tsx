c997e900e66c240995e364d1ea2180c7e2c33028
SergeWilfried <willytheigeek@gmail.com>
2024-09-30 10:08:30 +0000

    feat: add a map view

---
+++ app/(protected)/admin/map/page.tsx
@@ -0,0 +1,26 @@
+import mapboxgl from "mapbox-gl";
+import "mapbox-gl/dist/mapbox-gl.css";
+import { useEffect, useRef } from "react";
+import styles from "../styles/index.module.scss";
+
+export default function Maps() {
+  const mapContainer = useRef<any>(null);
+  const map = useRef<mapboxgl.Map | any>(null);
+
+  useEffect(() => {
+    mapboxgl.accessToken = process.env.MAPBOX_ACCESS_TOKEN ?? "";
+
+    map.current = new mapboxgl.Map({
+      container: mapContainer.current,
+      style: "mapbox://styles/mapbox/satellite-v9",
+      center: [-1.46389, 53.296543],
+      zoom: 13,
+    });
+  }, []);
+
+  return (
+    <div id="map">
+      <div className={styles.style1} ref={mapContainer} />
+    </div>
+  );
+}
