bf90a7056ac0a73ab2204e1710c7b4e1cf3c8974
SergeWilfried <willytheigeek@gmail.com>
2024-10-06 15:51:56 +0000

    fix: email verification step

--- components/multistep/registration/step5.tsx
+++ components/multistep/registration/step5.tsx
@@ -1,21 +1,28 @@
 "use client";
 
-import { Button } from '@/components/ui/button';
 import React from 'react';
+import { CardTitle } from "@/components/ui/card";
+import { Button } from '@/components/ui/button';
 
 interface CheckEmailStepProps {
   onPrevious: () => void;
 }
 
-const CheckEmailStep: React.FC<CheckEmailStepProps> = ({ onPrevious }) => {
-  return (
-    <div>
-      <h2>Check Your Email</h2>
-      <p>We sent you an email with further instructions.</p>
-      <Button onClick={onPrevious}>Back</Button>
-      <Button>Open Email App</Button>
+const CheckEmailStep = React.memo(({ onPrevious }: CheckEmailStepProps) => (
+  <>
+    <CardTitle>Vérifiez votre e-mail</CardTitle>
+    <div className="mt-4 grid w-full items-center gap-4">
+      <p className="text-sm text-gray-500">
+        Nous vous avons envoyé un e-mail avec des instructions supplémentaires. Veuillez vérifier votre boîte de réception.
+      </p>
+      <div className="flex justify-between mt-6">
+        <Button variant="outline" onClick={onPrevious}>Retour</Button>
+        <Button>Ouvrir l'application e-mail</Button>
+      </div>
     </div>
-  );
-};
+  </>
+))
+
+CheckEmailStep.displayName = 'CheckEmailStep';
 
 export default CheckEmailStep;
